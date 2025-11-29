eafcd81f83b6ff97115f703943372d49d834c852
charles-future <charles.future0x77@outlook.com>
2025-11-29 00:33:43 -0700

    chore: comment update (docs/styles-5429) (#1)
    * added comment (docs/styles-5429)
    
    * removed comment (docs/styles-5429)

--- publish.py
+++ publish.py
@@ -1,4 +1,3 @@
-import subprocess
 
 
 def run_command(command):
@@ -22,3 +21,4 @@ print("Publishing the package to PyPI...")
 run_command("poetry publish")
 
 print("Done!")
+# auto-comment Sat 11/29/2025  2:32:51 (docs/styles-5429) 
