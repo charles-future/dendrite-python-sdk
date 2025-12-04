8497369560a76c157e4da1844c5efbe101819c87
Sina Moradi <charles.future0x77@outlook.com>
2025-12-04 11:58:34 +0330

    Chore/styles 1940 (#2)
    * removed comment (chore/styles-1940)
    
    * removed comment (chore/styles-1940)
    
    * removed comment (chore/styles-1940)

--- publish.py
+++ publish.py
@@ -1,6 +1,3 @@
-
-
-def run_command(command):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         print(f"Error: {result.stderr}")
