0b5c56b4efc33207b50f2dbb723873da1ddc23c0
Sina Moradi <charles.future0x77@outlook.com>
2025-12-07 07:37:05 +0330

    Chore/UI 5468 (#3)
    * removed comment (chore/ui-5468)
    
    * removed comment (chore/ui-5468)
    
    * removed comment (chore/ui-5468)
    
    * removed comment (chore/ui-5468)

--- publish.py
+++ publish.py
@@ -1,7 +1,3 @@
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    if result.returncode != 0:
-        print(f"Error: {result.stderr}")
-    else:
         print(result.stdout)
 
 
